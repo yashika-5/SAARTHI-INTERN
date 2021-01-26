2456d0ec98f412634024e050f04a1ef7650a6cdc
yashika-5 <yashikagupta2021@gmail.com>
2021-01-26 12:10:52 +0530

    Scroll Updated

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import { EmployeeService } from '../services/employee.service';
 })
 export class HomeComponent implements OnInit {
   headers= ['EmployeeName','EmployeeId','Department', 'Email', 'Date of Joining','Delete'];
-  users :any=[{empName: "nk", empId: 1, department: "Marketing", emailId: "asd@jds.c", doj: "1020-04-05"},{empName: "nk", empId: 1, department: "Marketing", emailId: "asd@jds.c", doj: "1020-04-05"},{empName: "nk", empId: 1, department: "Marketing", emailId: "asd@jds.c", doj: "1020-04-05"},{empName: "oiu", empId: 2, department: "Front End", emailId: "sada@sad.co", doj: "0009-07-07"},{empName: "kjh", empId: 4, department: "Testing", emailId: "absd@sdas.co", doj: "2010-07-08"},{empName: "kjh", empId: 4, department: "Testing", emailId: "absd@sdas.co", doj: "2010-07-08"}];
+  users :any=[{empName: "Rahul", empId: 1, department: "Marketing", emailId: "rahul@jds.com", doj: "2016-04-05"},{empName: "Ram", empId: 2, department: "Marketing", emailId: "ram@gmail.in", doj: "2019-04-05"},{empName: "Yashu", empId: 3, department: "Marketing", emailId: "yashi@p.in", doj: "2017-04-05"},{empName: "Nishu", empId: 4, department: "Front End", emailId: "sada@sad.co", doj: "0009-07-07"},{empName: "Nikhil", empId: 5, department: "Testing", emailId: "absd@sdas.co", doj: "2010-07-08"},{empName: "Sam", empId: 6, department: "Testing", emailId: "absd@sdas.co", doj: "2010-07-08"}];
 
   constructor(private employeService: EmployeeService) { }
 
